285edd8a8d7492908ba81b431f4f693b951580e6
jeongsooyoung <code.ameba@gmail.com>
2019-06-19 12:44:12 +0900

    clock modified

--- analog-clock/clock.component.ts
+++ analog-clock/clock.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { TimeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-clock',
   template: `
   <div class="clock">
     <div class="analog-clock">
+    <!-- [ngStyle] 프로퍼티 바인딩으로 바늘에 css 적용 -->
       <div class="hour hand" [ngStyle]="hourHand"></div> <!-- 시침 -->
       <div class="minute hand" [ngStyle]="minHand"></div> <!-- 분침 -->
       <div class="second hand" [ngStyle]="secHand"></div> <!-- 초침 -->
@@ -18,6 +18,7 @@ import { TimeInterval } from 'rxjs';
 })
 export class ClockComponent {
 
+  // 타입 지정
   now: Date;
   sec: number;
   min: number;
@@ -29,18 +30,22 @@ export class ClockComponent {
   minHand: object;
   hourHand: object;
 
+  // 시간 데이터를 setInterval() 사용하여 1초마다 지속적 갱신
   realTime = setInterval(() => {
     this.nowTime();
   }, 1000);
 
   nowTime() {
+      // Date()로 시간정보 취득 후 초, 분, 시 각각 할당
       this.now = new Date();
       this.sec = this.now.getSeconds();
       this.min = this.now.getMinutes();
       this.hour = this.now.getHours();
+      // 디지털시계 문자열 할당, 삼항연산자 사용하여 한자리수일 때에는 앞에 0 붙임.
       this.secDigital = this.sec < 10 ? `0${this.sec}` : `${this.sec}`;
       this.minDigital = this.min < 10 ? `0${this.min}` : `${this.min}`;
       this.hourDigital = this.hour < 10 ? `0${this.hour}` : `${this.hour}`;
+      // 아날로그 시계에 css 객체 할당
       this.secHand = { transform : `rotate(${this.sec * 6}deg)`};
       this.minHand = { transform : `rotate(${this.min * 6}deg)`};
       this.hourHand = { transform : `rotate(${(this.hour % 12) * 30}deg)`};
